44a6fb3cd6ff5002474c9f76145f599fe4aadf5c
JonathanZhong <965754886@qq.com>
2019-04-04 15:47:04 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+import lombok.*;
+
+/**
+ * @author Jonathan
+ * @since 2019-04-04
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
---
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.example.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.User;
+
+/**
+ * @author Jonathan
+ * @since 2019-04-04
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
